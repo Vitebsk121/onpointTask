de277d608e54b2a61b07f2dfb2208499d0ade56d
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2022-05-08 17:34:13 +0300

    feat: данные о картинках вынесены из компонентов слайдов в отдельные файлы

---
+++ src/data/imagesFirstSlide.ts
@@ -0,0 +1,50 @@
+import {IimagesData} from "../../interfaces/IimagesData";
+import styles from "../components/SlideFirst/SlideFirst.module.scss";
+
+export const imagesData: IimagesData[] = [
+  {
+    url: '../../assets/images/slide1/molecule.png',
+    alt: 'molecule',
+    cls: [styles.pic, styles.molecule],
+  },
+  {
+    url: '../../assets/images/slide1/bacteria.png',
+    alt: 'bacteria',
+    cls: [styles.pic, styles.bacteria],
+  },
+  {
+    url: '../../assets/images/slide1/bacteria1.png',
+    alt: 'bacteria',
+    cls: [styles.pic, styles.bacteria1],
+  },
+  {
+    url: '../../assets/images/slide1/bacteria2.png',
+    alt: 'bacteria',
+    cls: [styles.pic, styles.bacteria2],
+  },
+  {
+    url: '../../assets/images/slide1/bacteria3.png',
+    alt: 'bacteria',
+    cls: [styles.pic, styles.bacteria3],
+  },
+  {
+    url: '../../assets/images/slide1/bacteria4.png',
+    alt: 'bacteria',
+    cls: [styles.pic, styles.bacteria4],
+  },
+  {
+    url: '../../assets/images/slide1/bacteria5.png',
+    alt: 'bacteria',
+    cls: [styles.pic, styles.bacteria5],
+  },
+  {
+    url: '../../assets/images/slide1/pink_sperm.png',
+    alt: 'pink sperm',
+    cls: [styles.pic, styles.sperm],
+  },
+  {
+    url: '../../assets/images/slide1/pink_sperm_1.png',
+    alt: 'pink sperm',
+    cls: [styles.pic, styles.sperm1],
+  },
+];
---
+++ src/data/imagesThirdSlide.ts
@@ -0,0 +1,50 @@
+import {IimagesData} from "../../interfaces/IimagesData";
+import styles from "../components/SlideThird/SlideThird.module.scss";
+
+export const imagesData: IimagesData[] = [
+  {
+    url: '../../assets/images/slide3/bottle.png',
+    alt: 'bottle',
+    cls: [styles.pic, styles.bottle],
+  },
+  {
+    url: '../../assets/images/slide3/bubble_blue.png',
+    alt: 'blue bubble',
+    cls: [styles.pic, styles.bubble1],
+  },
+  {
+    url: '../../assets/images/slide3/bubble_blue.png',
+    alt: 'blue bubble',
+    cls: [styles.pic, styles.bubble2],
+  },
+  {
+    url: '../../assets/images/slide3/bubble_red.png',
+    alt: 'red bubble',
+    cls: [styles.pic, styles.bubble3],
+  },
+  {
+    url: '../../assets/images/slide3/bubble_pink.png',
+    alt: 'pink bubble',
+    cls: [styles.pic, styles.bubble4],
+  },
+  {
+    url: '../../assets/images/slide3/bubble_blue.png',
+    alt: 'blue bubble',
+    cls: [styles.pic, styles.bubble5],
+  },
+  {
+    url: '../../assets/images/slide3/bubble_pink.png',
+    alt: 'pink bubble',
+    cls: [styles.pic, styles.bubble6],
+  },
+  {
+    url: '../../assets/images/slide3/bubble_blue.png',
+    alt: 'blue bubble',
+    cls: [styles.pic, styles.bubble7],
+  },
+  {
+    url: '../../assets/images/slide3/bubble_darkBlue.png',
+    alt: 'dark blue bubble',
+    cls: [styles.pic, styles.bubble8],
+  },
+];
